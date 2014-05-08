8f2e14e3e3c9a318393ef36782e04cd96bd8ad0a
Dra <dara_r@rambler.ru>
2014-05-08 12:38:52 +0300

    test commit

--- WebContentConsole/Program.cs
+++ WebContentConsole/Program.cs
@@ -11,8 +11,9 @@ namespace WebContentConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start program");
             Configuration.Configuration.ConfigureAutofac();
-
+            
             Console.WriteLine("End a program");
             Console.ReadLine();
         }
